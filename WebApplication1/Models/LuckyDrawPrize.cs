f64ddc7bdff48466b314e2ba2e15a818c9666d48
shengyao1026 <shengyao1026@gmail.com>
2026-02-01 19:39:44 +0800

    Define lucky draw prize model

---
+++ WebApplication1/Models/LuckyDrawPrize.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class LuckyDrawPrize
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public int EventSlotId { get; set; }
+        public int ProgrammeId { get; set; }
+        public int Sequence { get; set; }
+        public string? PrizeName { get; set; }
+        public string? PrizeImageName { get; set; }
+        public int PrizeCount { get; set; }
+        public string? Counter { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool RequireGold { get; set; }
+
+    }
+}
