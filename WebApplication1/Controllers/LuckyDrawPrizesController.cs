0a5424eecaa091eb32f74189cc2642eb78523bd7
shengyao1026 <shengyao1026@gmail.com>
2026-02-01 19:42:43 +0800

    feat: implement LuckyDrawPrizesController

---
+++ WebApplication1/Controllers/LuckyDrawPrizesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/lucky-draw-prizes")]
+    public class LuckyDrawPrizesController : ControllerBase
+    {
+        private readonly PrizeRepository _repo;
+
+        public LuckyDrawPrizesController(PrizeRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_repo.GetAllPrizes());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var prize = _repo.GetPrizeById(id);
+
+            if (prize == null)
+            {
+                return NotFound(new { message = "Prize not found!" });
+            }
+
+            return Ok(prize);
+        }
+    }
+}
